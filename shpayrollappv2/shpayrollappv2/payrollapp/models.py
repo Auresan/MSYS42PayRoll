794089a8f1f3c98dd3763d197c604fe91f57a012
Auresan <auresanisgold@gmail.com>
2024-02-18 16:18:16 +0800

    Updates to Models

--- shpayrollappv2/shpayrollappv2/payrollapp/models.py
+++ shpayrollappv2/shpayrollappv2/payrollapp/models.py
@@ -5,7 +5,7 @@ class Employee(models.Model):
     Last_name = models.CharField(max_length=255)
     First_name = models.CharField(max_length=255)
     Middle_name = models.CharField(max_length=255)
-    id_number = models.CharField(max_length=6)
+    id_number = models.CharField(max_length=6, primary_key=True)
     Status  = models.CharField(max_length=255)
     Department  = models.CharField(max_length=255)
     Role   = models.CharField(max_length=255)
@@ -18,10 +18,10 @@ class Employee(models.Model):
 
 
     
-    ColaEarnings  = models.FloatField()
-    CAdeductions   = models.FloatField()
-    COOPdeductions  = models.FloatField()
-    ULdeductions   = models.FloatField()
+    #ColaEarnings  = models.FloatField()
+    #CAdeductions   = models.FloatField()
+    #COOPdeductions  = models.FloatField()
+    #ULdeductions   = models.FloatField()
     def getID(self):
         return self.id_number
     def __str__(self):
@@ -102,11 +102,64 @@ class Leave(models.Model):
     Start_Date = models.DateField(blank=True, null=True)
     End_Date = models.DateField(blank=True, null=True)
     def getID(self):
-        return self.Holiday_ID
+        return self.Leave_ID
     def __str__(self):
         z = str(self.pk)+':'+str(self.getID())
         return z
-    
+
+class SSS(models.Model):
+    SSS_Rate_ID = models.IntegerField()
+    Regular_SS_Employer_Rate = models.FloatField()
+    Regular_SS_Employee_Rate = models.FloatField()
+    EC_Contribution = models.FloatField()
+    WISP_Employer_Rate = models.FloatField()
+    WISP_Employee_Rate = models.FloatField()
+    Type = models.CharField(max_length=255)
+    Total_Contribution = models.FloatField()
+    Start_Range = models.FloatField()
+    End_Range = models.FloatField()
+    def getID(self):
+        return self.SSS_Rate_ID
+    def __str__(self):
+        z = str(self.pk)+':'+str(self.getID())
+        return z
+
+class PhilHealth(models.Model):
+    PhilHealth_Rate_ID = models.IntegerField()
+    Employer_Rate = models.FloatField()
+    Employee_Rate = models.FloatField()
+    Start_Range = models.FloatField()
+    End_Range = models.FloatField()
+    def getID(self):
+        return self.PhilHealth_Rate_ID
+    def __str__(self):
+        z = str(self.pk)+':'+str(self.getID())
+        return z
+
+class HDMF(models.Model):
+    HDMF_Rate_ID = models.IntegerField()
+    Employer_Rate = models.FloatField()
+    Employee_Rate = models.FloatField()
+    Start_Range = models.FloatField()
+    End_Range = models.FloatField()
+    def getID(self):
+        return self.HDMF_Rate_ID
+    def __str__(self):
+        z = str(self.pk)+':'+str(self.getID())
+        return z
+
+class WitholdingTax(models.Model):
+    WTAX_Rate_ID = models.IntegerField()
+    Fix_Tax_Amount = models.FloatField()
+    Tax_Rate_On_Excess = models.FloatField()
+    Start_Range = models.FloatField()
+    End_Range = models.FloatField()
+    def getID(self):
+        return self.WTAX_Rate_ID
+    def __str__(self):
+        z = str(self.pk)+':'+str(self.getID())
+        return z
+
 class Payslip_Transaction(models.Model):
     Transaction_ID = models.IntegerField()
     Date_Distributed = models.DateField(blank=True, null=True)
@@ -115,19 +168,19 @@ class Payslip_Transaction(models.Model):
     Net_Pay = models.FloatField()
     Absence_Deductions = models.FloatField()
     Employee_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    SSS_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    PhilHealth_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    HDMF_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    WTax_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    HMO_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    ULDeductions_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    CA_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    COOP_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    COLA_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    AddtlEarning_Rate_ID = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    SSS_Rate_ID = models.ForeignKey(SSS, on_delete=models.CASCADE)
+    PhilHealth_Rate_ID = models.ForeignKey(PhilHealth, on_delete=models.CASCADE)
+    HDMF_Rate_ID = models.ForeignKey(HDMF, on_delete=models.CASCADE)
+    WTax_Rate_ID = models.ForeignKey(WitholdingTax, on_delete=models.CASCADE)
+    HMO_Rate_ID = models.ForeignKey(HMO, on_delete=models.CASCADE)
+    ULDeductions_Rate_ID = models.ForeignKey(UNIFORMLAPTOPDEDUCTIONS, on_delete=models.CASCADE)
+    CA_Rate_ID = models.ForeignKey(CA, on_delete=models.CASCADE)
+    COOP_Rate_ID = models.ForeignKey(COOP, on_delete=models.CASCADE)
+    COLA_Rate_ID = models.ForeignKey(COLA, on_delete=models.CASCADE)
+    AddtlEarning_Rate_ID = models.ForeignKey(ADDITIONAL_EARNINGS, on_delete=models.CASCADE)
 
     def getID(self):
-        return self.Holiday_ID
+        return self.Transaction_ID
     def __str__(self):
         z = str(self.pk)+':'+str(self.getID())
         return z
@@ -142,7 +195,7 @@ class ATTENDANCE_HISTORY(models.Model):
     IN_OUT = models.CharField(max_length=255)
     Date = models.DateField(blank=True, null=True)
     def getID(self):
-        return self.Attendance_ID
+        return self.History_ID
     def __str__(self):
         z = str(self.pk)+':'+str(self.getID())
         return z
