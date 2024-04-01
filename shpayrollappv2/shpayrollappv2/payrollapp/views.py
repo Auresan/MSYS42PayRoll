11e615b2cddd7b0ae0f25ae6a2df813006596013
Auresan <94361056+Auresan@users.noreply.github.com>
2024-04-02 02:51:42 +0800

    Tax Module
    Tax Module is done, will need to do massive rewrites to the codebase the day before due to certain issues im catching right now

--- shpayrollappv2/shpayrollappv2/payrollapp/views.py
+++ shpayrollappv2/shpayrollappv2/payrollapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.contrib import messages
 from django.utils import timezone
+from .taxEmployee_salary import *
 
 
 # Create your views here.
